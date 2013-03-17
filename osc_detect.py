1f03bb5365002a276a0aaad3c6f2f98f75f103ab
Jack Ireland <jack.ireland@nasa.gov>
2013-03-17 09:05:53 -0400

    fixed the fourier transform results

--- osc_detect.py
+++ osc_detect.py
@@ -2,6 +2,7 @@ import numpy as np
 import datetime
 from sunpy.lightcurve import LightCurve
 from sunpy.map import MapCube
+from sunpy.map import Map
 
 class BTS():
     """Bayesian Spectral and Parameter Estimation time-series object.
@@ -28,8 +29,17 @@ class BTS():
         if isinstance(input, MapCube):
             self.data = [m.data for m in input]
             self.index = [m.date for m in input]
+            
+        # Get the header
+        if "header" in kwargs:
+            self.header = header
+        elif isinstance(input, LightCurve):
+            self.header = input.header
+        elif isinstance(input, MapCube):
+            self.header = input[0].header
+        else:
+            self.header = None
 
-        
         # get the times relative to the start time
         if isinstance(self.index[0], datetime.datetime):
             self.time = (self.index[:]-self.index[0]).total_seconds()
@@ -53,6 +63,8 @@ class BTS():
         self.shape = self.data.shape
         self.nmap = self.shape[-1]
 
+        self.input_type = input.__class__.__name__
+
     def where_are_the_low_frequencies(self, limit=0.01):
         """ Returns True for frequencies that are below the low frequency limit"""
         return np.array(self.angular_frequencies*self.time[-1]) < limit
@@ -63,42 +75,47 @@ class BTS():
         
         # limit below which we are analyzing at a "low frequency"
         self.limit = kwargs.get("limit", 0.01)
-        
+        watlf = self.where_are_the_low_frequencies()
+
+        # Calculate the period-dependent part of the probability expression
         if "frequencies" in kwargs:
             self.frequencies = kwargs.get("frequencies")
             self.angular_frequencies = 2*np.pi*self.frequencies
             cw = _schuster()
         else:
-            cw = (np.abs(np.fft.fft(data,axis=-1)))**2
-            
-        watlf = self.where_are_the_low_frequencies()
-        
-        
+            cw = ((np.abs(np.fft.fft(self.data,axis=-1)))**2)/self.nmap
+        cw = cw[...,0:len(self.frequencies)]
+
+        # Calculate the PDF
         if "noise_variance" in kwargs:
             pdf = exp(cw/kwargs.get("noise_variance"))
         else:
-            d2 = np.mean(self.data^2,axis=-1)
-            z = np.zeros(size=cw.shape)
-            for i in range(0,self.nmap+1):
-                z[:,:,i] = cw[:,:,i]/d2[:,:]
-            pdf = (1.0 - 2.0*np.array(z)(1.0*self.nmap)**(1.0-self.nmap/2.0))
+            d2 = np.mean(self.data**2,axis=-1)
+            z = np.zeros_like(cw)
+            for i in range(0,z.shape[-1]):
+                z[...,i] = (2.0/self.nmap)*cw[...,i]/d2[...]
+            pdf = (1.0 - z)**(1.0-self.nmap/2.0)
         
         # The formulae used are only valid when there is no evidence of a low
         # frequency.  Test for evidence of a low frequency component and issue
         # a warning if it is there.  If not
         total_pdf = np.sum(pdf, axis=-1)
-        for i in range(0,self.nmap+1):
-            pdf[:,:,i] = pdf[:,:,i]/total_pdf[:,:]
+        for i in range(0,pdf.shape[-1]):
+            pdf[...,i] = pdf[...,i]/total_pdf[...]
 
         # The power at each FFT frequency is its own map.  Make a list of maps
         # with the appropriate header and 
-        maplist = []
-        for i in range(0,self.nmap+1):
-            m = Map
-            maplist.append(m)
-        pdf_mapcube = MapCube(maplist, ordering = self.fft_frequencies)
-            
-        return pdf_mapcube
+        if len(self.shape) == 3 and self.input_type == 'MapCube':
+            maplist = []
+            for i in range(0,self.nmap+1):
+                m = Map(pdf[:,:,i], header=self.header)
+                maplist.append(m)
+            pdf_answer = MapCube(maplist, ordering=self.frequencies)
+        else:
+            pdf_answer = {"pdf":pdf,
+                          "frequencies":self.frequencies,
+                          "header":self.header}
+        return pdf_answer
 
     def _schuster(self):
         """Return the Schuster periodogram"""
--- test_osc_detect.py
+++ test_osc_detect.py
@@ -1,11 +1,22 @@
-import sunpy
+from matplotlib import pyplot as plt
+
 import osc_detect
 import numpy as np
 
-nt = 200
-start = 104
-
-dc = np.random.normal(size=(100,101,nt))
-index = 12.0*np.arange(start, start+nt+1)
-
-print osc_detect.BTS(dc,index=index).nmap
+def main():
+    nt = 200
+    start = 104
+    
+    dc = np.random.normal(size=(100,101,nt))
+    index = 12.0*np.arange(start, start+nt+1)
+    for i in range(0,nt):
+        dc[10,10,i] = dc[10,10,i] + np.sin(2*index[i]*np.pi/180.0)
+    dcmean = np.mean(dc,axis=-1)
+    for i in range(0,nt):
+        dc[...,i] = dc[...,i] - dcmean[...]
+    plt.plot(dc[10,10,:])
+    
+    return osc_detect.BTS(dc,index=index).get_pdf()
+    
+if __name__ == '__main__':
+    main()
