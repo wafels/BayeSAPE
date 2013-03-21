8e51c84a7bc77ef8c3b59f17b1e347f1b76ca504
Jack Ireland <jackireland@gmail.com>
2013-03-21 15:39:02 -0400

    new testing for AIA data

---
+++ osc_detect_aia.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+
+import osc_detect 
+import numpy as np
+import sunpy
+
+def main():
+    directory = '/home/ireland/Data/AIA_Data/test2/'
+    
+    mc = sunpy.make_map(directory, derotate='by_center_of_fov')
+    
+    return mc
+    
+if __name__ == '__main__':
+    main()
